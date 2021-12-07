13d799219fccc8dbbc5b0f315221d464937e5606
alaedine07 <alaedineboufaden@gmail.com>
2021-12-07 14:23:07 +0100

    lockboxes solve

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """ lockboxes solver """
+    l = len(boxes)
+    d = {}
+    d[0] = "unlocked"
+    for index in range(1, l):
+        d[index] = "locked"
+    keys = []
+    for key in boxes[0]:
+        keys.append(key)
+    for index in range(1, l):
+        if index in keys:
+            for e in boxes[index]:
+                if e not in keys and e != 0 and e is not None:
+                    keys.append(e)
+            d[index] = "unlocked"
+    key = 0
+    while (key < len(d) - 1):
+        if d[key] == "locked":
+            if key in keys:
+                for e in boxes[key]:
+                    if e not in keys and e != 0 and e is not None:
+                        keys.append(e)
+                d[key] = "unlocked"
+                key = 0
+        key += 1
+    if ("locked" in d.values()):
+        return False
+    else:
+        return True
