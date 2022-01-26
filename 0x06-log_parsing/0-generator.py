47c5735bcf1022225d04c04cb67dfd8722ff2bad
alaedine07 <alaedineboufaden@gmail.com>
2022-01-26 14:31:38 +0100

    modify generator

--- 0x06-log_parsing/0-generator.py
+++ 0x06-log_parsing/0-generator.py
@@ -3,7 +3,6 @@ import random
 import sys
 from time import sleep
 import datetime
-import os
 
 for i in range(10000):
     sleep(random.random())
@@ -13,11 +12,4 @@ for i in range(10000):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
-    try:
-        sys.stdout.flush()
-    except BrokenPipeError:
-        # Python flushes standard streams on exit; redirect remaining output
-        # to devnull to avoid another BrokenPipeError at shutdown
-        devnull = os.open(os.devnull, os.O_WRONLY)
-        os.dup2(devnull, sys.stdout.fileno())
-        sys.exit(1)
+    sys.stdout.flush()
